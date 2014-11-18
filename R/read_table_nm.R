47ffdd3c80f9a278163ffa636fb9b3b391a3d392
ronkeizer <ronkeizer@gmail.com>
2014-11-18 14:56:10 -0800

    about 5x faster reading of tables using data.frame::fread, attempt fread and fallback to other approaches if not successful.

--- R/read_table_nm.R
+++ R/read_table_nm.R
@@ -9,26 +9,40 @@
 
 read_table_nm <- function(file, perl = NULL) {
   file <- gsub("~", path.expand("~"), file)
-  if(is.null(perl)) { 
-    perl <- FALSE
-    chk <- try(system("perl -v", intern = TRUE, ignore.stderr = TRUE), silent = TRUE)
-    if(length(grep("Copyright", chk)) > 0) {
-      perl <- TRUE
-    }
-  }
-  if (file.exists(file)) {
-    if (perl) {
-      cmd <- paste0("perl -e 'open (IN, \"<", file, "\"); my $i = 0; my $cols = 0; while (my $line = <IN>) { if ($line =~ m/[a-df-z]/i) { unless($line =~ m/^TABLE NO/ || $cols == 1) { print $line; $cols = 1; } } else { print $line } } ; close(IN);'")
-      tab <- read.table(pipe(cmd), header=T);            
+  tab_fr <- try(data.table::fread(file), silent = TRUE)
+  if ("data.frame" %in% class(tab_fr)) {
+    tab <- data.frame(tab_fr)
+    colnames(tab) <- gsub(" ", "", colnames(tab_fr))
+    sel <- grep("[A-DF-Z]", tab[,1])
+    if(length(sel)>0) {
+      return(data.frame(apply(tab[-sel,], 2, as.numeric))) 
     } else {
-      tab <- readLines (file)      
-      del_rows <- c(grep("TABLE", tab), grep ("TABLE", tab)[-1] + 1)
-      if(length(del_rows) > 0) {
-        tab <- tab[-del_rows]        
-      }
-      read.table(textConnection(tab), skip=1, header=T)               
-    }        
+      return(tab)
+    }
   } else {
-    stop ("File does not exist!")
+    cat ("Tip: NONMEM tables can be read much faster when 'NOTITLE FORMAT=,1PE11.4' is used in $TABLE")
+    if(is.null(perl)) { 
+      perl <- FALSE
+      chk <- try(system("perl -v", intern = TRUE, ignore.stderr = TRUE), silent = TRUE)
+      if(length(grep("Copyright", chk)) > 0) {
+        perl <- TRUE
+      }
+    }
+    if (file.exists(file)) {
+      if (perl) {
+        cmd <- paste0("perl -e 'open (IN, \"<", file, "\"); my $i = 0; my $cols = 0; while (my $line = <IN>) { if ($line =~ m/[a-df-z]/i) { unless($line =~ m/^TABLE NO/ || $cols == 1) { print $line; $cols = 1; } } else { print $line } } ; close(IN);'")
+        tab <- read.table(pipe(cmd), header=T);            
+      } else {
+        cat("Warning: Perl not found either, resorting to slowest method for reading tables.")
+        tab <- readLines (file)      
+        del_rows <- c(grep("TABLE", tab), grep ("TABLE", tab)[-1] + 1)
+        if(length(del_rows) > 0) {
+          tab <- tab[-del_rows]        
+        }
+        read.table(textConnection(tab), skip=1, header=T)               
+      }        
+    } else {
+      stop ("File does not exist!")
+    }    
   }
 }
