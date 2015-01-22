2dbf210c0ec8ce0c7a14bd2a7af2b4024cc26a81
Devin Pastoor <devin.pastoor@gmail.com>
2015-01-21 20:44:32 -0800

    add replace_list_elements

---
+++ R/replace-list-elements.R
@@ -0,0 +1,24 @@
+#' replace list elements by name
+#' @param list original list
+#' @param replacement replacement list
+#' @details
+#' Finds and replaces list elements by name and throws an error if an 
+#'    element is not available in the original list. This is a local duplicate
+#'    of the PKPDmisc copy for the VPC package to reduce dependency on PKPDmisc
+#'    at this time.
+#' @examples 
+#' \dontrun{
+#' list <- list(dv = "dv", idv = "idv")
+#' replacement <- list(dv = "conc", idv = "time")
+#' list <- replace_list_elements(list, replacement)
+#' }
+#' @export
+replace_list_elements <- function(list, replacement) {
+  missing <- which(!names(replacement) %in% names(list))
+  if(length(missing) != 0) {
+    warning(paste("Nothing named: ", paste(names(replacement)[missing], collapse= ", ", "found to replace") ))
+    replacement <- replacement[-missing]
+  }
+  list[names(replacement)] <- lapply(names(replacement), function(x) list[[x]] <- replacement[[x]])
+  return(list)
+}
