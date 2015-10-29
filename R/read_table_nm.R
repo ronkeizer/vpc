e6c681fb8d8c2821b3c44d0d0d5ded4dbc790cc2
ronkeizer <ronkeizer@gmail.com>
2015-10-29 10:25:27 -0700

    update docs; update read_table_nm;

--- R/read_table_nm.R
+++ R/read_table_nm.R
@@ -1,49 +1,84 @@
-#' Read table file simulated with NONMEM
-#' 
-#' \code{read_table_nm} will read any table simulated from NONMEM in the $TABLE record. Tables can be simulated using either ONEHEADER or NOHEADER, and can be single or multiple subproblems.
-#' @param file The file to be read
-#' @param perl a boolean or NULL variable indicating whether to use Perl to read in the file. Default is NULL, which will make the function check whether Perl is available. Use of Perl is advised as it is much faster.
+#' NONMEM output table import function
+#'
+#' @description Quickly import NONMEM output tables into R. 
+#' Function taken from `modelviz` package by Benjamin Guiastrennec.
+#' When both \code{skip} and \code{header} are \code{NULL},
+#' \code{read_nmtab} will automatically detect the optimal
+#' settings to import the tables. When more than one files are
+#' provided for a same NONMEM run, they will be combined into
+#' a single \code{data.frame}.
+#'
+#' @param file full file name
+#' @param skip number of lines to skip before reading data
+#' @param header logical value indicating whether the file contains the names
+#' of the variables as its first line
+#' @param rm_duplicates logical value indicating whether duplicated columns should be removed
+#' @param nonmem_tab logical value indicading to the function whether the file is a
+#' table or a nonmem additional output file.
+#'
+#' @seealso \code{\link{parse_nonmem_model}}, \code{\link{import_qmd_info}}
+#' @return A \code{data.frame}
+#' @examples
+#' \dontrun{
+#' data <- read_table_nm(file = '../models/pk/sdtab101')
+#' }
 #' @export
-#' @return a data.frame
-#' @seealso \link{vpc}
-
-read_table_nm <- function(file, perl = NULL) {
-  file <- gsub("~", path.expand("~"), file)
-  if (file.exists(file)) {
-    test <- suppressWarnings(try(data.table::fread(file, nrows=3), silent = TRUE))
-    if ("data.frame" %in% class(test) & !is.na(unlist(test[1])[1])) {
-      tab_fr <- suppressWarnings(try(data.table::fread(file), silent = TRUE))
-      data.table::setnames(tab_fr, gsub(" ", "", colnames(tab_fr)))
-      sel <- grep("[A-DF-Z]", tab_fr[[1]])
-      if(length(sel)>0) {
-        return(data.frame(apply(tab_fr[-sel,], 2, as.numeric)))    
-      } else {
-        return(data.frame(apply(tab_fr, 2, as.numeric)))
-      }
+read_table_nm <- function(
+  file = NULL,
+  skip = NULL,
+  header = NULL,
+  rm_duplicates = FALSE,
+  nonmem_tab = TRUE) {
+    
+    # Check inputs
+    if(is.null(file)) {
+      stop('Argument \"file\" required.')
+    }
+    
+    if(!any(file.exists(file))) {
+      stop('No file not found.')
     } else {
-      cat ("Tip: NONMEM tables can be read much faster when 'NOTITLE FORMAT=,1PE11.4' is used in $TABLE\n")
-      if(is.null(perl)) { 
-        perl <- FALSE
-        chk <- try(system("perl -v", intern = TRUE, ignore.stderr = TRUE), silent = TRUE)
-        if(length(grep("Copyright", chk)) > 0) {
-          perl <- TRUE
-        }
+      file <- file[file.exists(file)]
+    }
+    
+    if(nonmem_tab) {
+      # If auto mode required
+      if(is.null(skip) & is.null(header)) {
+        test    <- readLines(file[1], n = 3)
+        skip    <- ifelse(grepl('TABLE NO', test[1]), 1, 0)
+        header  <- ifelse(grepl('[a-zA-Z]', test[2]), TRUE, FALSE)
       }
-      if (perl) {
-        cat("Using Perl to pre-parse data...\n")
-        cmd <- paste0("perl -e 'open (IN, \"<", file, "\"); my $i = 0; my $cols = 0; while (my $line = <IN>) { if ($line =~ m/[a-df-z]/i) { unless($line =~ m/^TABLE NO/ || $cols == 1) { print $line; $cols = 1; } } else { print $line } } ; close(IN);'")
-        tab <- read.table(pipe(cmd), header=T);            
-      } else {
-        cat("Warning: Perl not found either, resorting to slowest method for reading tables.\n")
-        tab <- readLines (file)      
-        del_rows <- c(grep("TABLE", tab), grep ("TABLE", tab)[-1] + 1)
-        if(length(del_rows) > 0) {
-          tab <- tab[-del_rows]        
-        }
-        read.table(textConnection(tab), skip=1, header=T)               
-      }        
-    }    
-  } else {
-    stop("File does not exist!")
-  }
-}
+      
+      # Import data
+      tab_file <- do.call('cbind', lapply(file, readr::read_table,
+                                          skip = skip, col_names = header))
+      
+      tab_file <- as.data.frame(apply(tab_file, MARGIN = 2, FUN = as.numeric))
+      
+      # Drop rows with NA (in simtab)
+      tab_file <- na.omit(tab_file)
+      
+      # Correct bug in the headers
+      if(header) {
+        colnames(tab_file)[grepl('\n',colnames(tab_file))] <-
+          gsub('\n.+', '', colnames(tab_file)[grepl('\n', colnames(tab_file))])
+      }
+      
+    } else {
+      # Search for final results only
+      skip     <- max(grep('TABLE NO', readLines(file[1])))
+      
+      # Import all files
+      tab_file <- do.call('cbind', lapply(file, read.table, skip = skip,
+                                          header = FALSE, fill = TRUE, as.is = TRUE))
+      colnames(tab_file) <- tab_file[1, ]
+      tab_file <- suppressWarnings(as.data.frame(apply(tab_file[-1, ], 2, as.numeric)))
+    }
+    
+    if(rm_duplicates) {
+      tab_file <- tab_file[, !duplicated(colnames(tab_file))]
+    }
+    
+    return(tab_file)
+    
+} 
