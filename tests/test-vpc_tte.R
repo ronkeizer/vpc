53b560c45a99723926e9d1b97902f21a77bd4c2d
ronkeizer <ronkeizer@gmail.com>
2017-10-24 11:06:37 -0700

    add test

--- tests/test-vpc_tte.R
+++ tests/test-vpc_tte.R
@@ -2,10 +2,14 @@ library(vpc)
 library(testit)
 Sys.setenv("R_TESTS" = "")
 
-obj <- vpc_tte(sim = vpc::rtte_sim_nm[1:200000,],  # stratified for covariate and study arm
+## Test for bug AR 20171025 column name simulation dataset
+test <- vpc::rtte_sim_nm[1:200000,]
+test$bla <- test$t
+test$t <- NULL
+obj <- vpc_tte(sim = test,  # stratified for covariate and study arm
                obs = vpc::rtte_obs_nm, 
                rtte = FALSE,
-               sim_cols = list(dv = "dv", idv = "t"), 
+               sim_cols = list(dv = "dv", idv = "bla"), 
                obs_cols = list(idv = "t"),
                labeller = ggplot2::label_both)
 
