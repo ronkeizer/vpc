16f0605f1838d7a6841117f18b05dea03d581612
ronkeizer <ronkeizer@gmail.com>
2018-08-06 14:36:28 -0700

    update docs; export create_vpc_theme()

--- R/create_vpc_theme.R
+++ R/create_vpc_theme.R
@@ -1,6 +1,7 @@
 #' Create new vpc theme
 #' 
 #' @param ... pass arguments to `new_vpc_theme`
+#' @export
 create_vpc_theme <- function(...) {
   message("Note: the `create_vpc_theme()` function has been renamed to `new_vpc_theme()`. Please update your scripts, `create_vpc_theme()` will be deprecated in future releases.")
   return(new_vpc_theme(...))
