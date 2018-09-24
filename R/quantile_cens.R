084d6a899dfa9d67110dd99744d1851f19aec2e2
Olivier Barriere <olivier.barriere@gmail.com>
2018-09-24 16:57:58 -0400

    ... other arguments to be passed to quantile function

--- R/quantile_cens.R
+++ R/quantile_cens.R
@@ -4,8 +4,10 @@
 #' @param p quantile
 #' @param limit censoring limit
 #' @param cens censoring direction (left/right)
+#' @param ... other arguments to be passed to quantile function
+#' 
 #' @export
-quantile_cens <- function(x, p = 0.5, limit = 1, cens = "left") {
+quantile_cens <- function(x, p = 0.5, limit = 1, cens = "left", ...) {
   if(cens %in% c("left", "lower", "bloq", "loq", "lloq")) {
     x[is.na(x)] <- -Inf
     x[x<limit] <- -Inf
@@ -13,7 +15,7 @@ quantile_cens <- function(x, p = 0.5, limit = 1, cens = "left") {
     x[is.na(x)] <- Inf
     x[x>limit] <- Inf
   }
-  q <- quantile(x, p)
+  q <- quantile(x, p, ...)
   ifelse(q %in% c(Inf, -Inf), NA, q)
 }
 
