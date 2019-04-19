30f8aadb274e7897534aeff9657330dbae253fec
ronkeizer <ronkeizer@gmail.com>
2019-04-19 13:50:09 -0700

    export quantile_cens

--- R/quantile_cens.R
+++ R/quantile_cens.R
@@ -5,6 +5,7 @@
 #' @param limit censoring limit
 #' @param cens censoring direction (left/right)
 #' 
+#' @export
 quantile_cens <- function(x, p = 0.5, limit = 1, cens = "left") {
   if(cens %in% c("left", "lower", "bloq", "loq", "lloq")) {
     x[is.na(x)] <- -Inf
