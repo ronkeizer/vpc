09b2c4a9d679696341a6e03a0944fd6f350aa762
Satya <sn248@cornell.edu>
2020-09-13 01:03:51 -0400

    Added scales to options
    Added the ability to define scale for x and y-axis  to be free or fixed

---
+++ tests/test-scales.R
@@ -0,0 +1,16 @@
+library(vpc)
+library(testit)
+Sys.setenv("R_TESTS" = "")
+
+tmp <- simple_data
+sex <- (tmp$obs$ID) %% 2
+race <- (tmp$obs$ID) %% 3
+tmp$obs$sex <- sex
+tmp$obs$race <- race
+tmp$sim$sex <- sex
+tmp$sim$race <- race
+
+v1 <- vpc(sim = tmp$sim, obs = tmp$obs, stratify = "race",
+          facet = "wrap", scales = "free_y", vpcdb = F, labeller = ggplot2::label_both)
+assert("plot succeeded", "ggplot" %in% class(v1))
+assert("facets succeeded", vpc:::is_equal(length(v1$facet$params$facets), 1))
