19f09d6a1d420429956e57826d4afe65bb27d32b
Ron Keizer <ron@insight-rx.com>
2021-02-05 10:15:53 -0800

    fix issue with labeler order in vpc_tte

---
+++ R/label_vpc_tte.R
@@ -0,0 +1,24 @@
+#' Custom ggplot2 labeller function.
+#' 
+#' Slight rewrite of ggplot::label_both, to make sure that labels 
+#' for events are ordered appropriately when doing facet_wrap.
+#' 
+label_vpc_tte <- function (labels, multi_line = TRUE, sep = ": ") {
+  value <- ggplot2:::label_value(labels, multi_line = multi_line)
+  variable <- ggplot2:::label_variable(labels, multi_line = multi_line)
+  if (multi_line) {
+    out <- vector("list", length(value))
+    for (i in seq_along(out)) {
+      tmp <- as.numeric(value[[i]])
+      value[[i]] <- as.character(tmp[order(tmp)])
+      out[[i]] <- paste(variable[[i]], value[[i]], sep = sep)
+    }
+  }
+  else {
+    value <- do.call("paste", c(value, sep = ", "))
+    variable <- do.call("paste", c(variable, sep = ", "))
+    out <- Map(paste, variable, value, sep = sep)
+    out <- list(unname(unlist(out)))
+  }
+  out
+}
