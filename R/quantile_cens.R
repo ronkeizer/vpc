981dd5af664ef7aef9a9851ddb926147908039ea
Bill Denney <wdenney@humanpredictions.com>
2021-04-13 05:50:44 -0400

    Enable quantile_cens() and loq_frac() to work with neither censoring direction to simplify other code

--- R/quantile_cens.R
+++ R/quantile_cens.R
@@ -1,11 +1,10 @@
 #' Calculate quantiles respecting the censored data
 #' 
 #' @inheritParams stats::quantile
-#' @param limit censoring limit
-#' @param cens censoring direction ("left"/"right")
+#' @inheritParams loq_frac
 #' @return The quantile of \code{x} treating \code{NA} values as censored
 #' @export
-quantile_cens <- function(x, probs = 0.5, limit = 1, cens = c("left", "right")) {
+quantile_cens <- function(x, probs = 0.5, limit = 1, cens = c("left", "right", "neither", "both")) {
   cens <- match.arg(cens)
   if (cens == "left") {
     x[is.na(x)] <- -Inf
@@ -13,6 +12,15 @@ quantile_cens <- function(x, probs = 0.5, limit = 1, cens = c("left", "right")) 
   } else if (cens == "right") {
     x[is.na(x)] <- Inf
     x[x>limit] <- Inf
+  } else if (cens == "neither") {
+    # TODO: Review on 2021-04: quantile_cens can now just work like quantile
+    # (simplifying other code that checks if quantile_cens() or quantile()
+    # should be used).  To consider: Should NA values be considered missing or
+    # an error?
+
+    # do nothing to x
+  } else if (cens == "both") {
+    stop("cens='both' is not yet supported") # nocov
   } else {
     stop("Invalid value for cens: ", cens) # nocov
   }
@@ -24,16 +32,30 @@ quantile_cens <- function(x, probs = 0.5, limit = 1, cens = c("left", "right")) 
 
 #' Calculate fraction of observations below lloq / above uloq
 #' 
-#' @param x data
-#' @param limit censoring limit
-#' @param cens censoring direction (left/right)
+#' @param x A numeric vector
+#' @param limit censoring limit (ignored if \code{cens="neither"})
+#' @param cens censoring direction
 #' @return The fraction of observations (\code{NA} is counted as below/above)
-loq_frac <- function(x, limit = 1, cens = c("left", "right")) {
+loq_frac <- function(x, limit = 1, cens = c("left", "right", "neither", "both")) {
   cens <- match.arg(cens)
   if (cens == "left") {
-    (sum(x < limit, na.rm=TRUE) + sum(is.na(x))) / length(x) 
+    if (length(limit) != 1) {
+      stop("limit must be a scalar if cens='left'")
+    }
+    (sum(x < limit, na.rm=TRUE) + sum(is.na(x))) / length(x)
   } else if (cens == "right") {
-    (sum(x > limit, na.rm=TRUE) + sum(is.na(x))) / length(x) 
+    if (length(limit) != 1) {
+      stop("limit must be a scalar if cens='right'")
+    }
+    (sum(x > limit, na.rm=TRUE) + sum(is.na(x))) / length(x)
+  } else if (cens == "neither") {
+    # Zero by definition if no censoring occurs
+    0
+  } else if (cens == "both") {
+    if (length(limit) != 2) {
+      stop("limit must have 2 elements if cens='both'")
+    }
+    (sum((x < min(limit)) | (x > max(limit)), na.rm=TRUE) + sum(is.na(x))) / length(x)
   } else {
     stop("Invalid value for cens: ", cens) # nocov
   }
--- tests/testit/test-quantile_cens.R
+++ tests/testit/test-quantile_cens.R
@@ -18,6 +18,25 @@ assert(
   vpc::quantile_cens(x=c(1:10, rep(NA, 10)), probs=0.3, cens="right", limit=6) %==%
     NA_real_
 )
+assert(
+  "quantile_cens returns the normal quantile value when neither direction is censored",
+  vpc::quantile_cens(x=1:10, probs=0.3, cens="neither") %==%
+    unname(quantile(x=1:10, probs=0.3))
+)
+assert(
+  "quantile_cens returns an error for trying to censor both directions",
+  tryCatch(
+    vpc::quantile_cens(x=1:10, probs=0.3, cens="both", limit=6),
+    error=function(e) e$message
+  ) == "cens='both' is not yet supported"
+)
+assert(
+  "quantile_cens returns an error for an invalid 'cens'",
+  class(try(
+    vpc::quantile_cens(x=1:10, probs=0.3, cens="foo", limit=6),
+    silent=TRUE
+  )) == "try-error"
+)
 
 assert(
   "loq_frac counts left values correctly",
@@ -28,6 +47,40 @@ assert(
   vpc:::loq_frac(x=1:10, limit=3, cens="right") == 0.7
 )
 assert(
-  "loq_frac only allows `cens` values of 'left' or 'right'",
+  "loq_frac only allows valid `cens` values",
   class(try(vpc:::loq_frac(x=1:10, limit=3, cens="foo"), silent=TRUE)) == "try-error"
+)
+assert(
+  "loq_frac counts neither direction values correctly",
+  vpc:::loq_frac(x=1:10, limit=NULL, cens="neither") == 0
+)
+assert(
+  "loq_frac counts neither direction values correctly (and ignores `limit`)",
+  vpc:::loq_frac(x=1:10, limit=1, cens="neither") == 0
+)
+assert(
+  "loq_frac counts both values correctly",
+  vpc:::loq_frac(x=1:10, limit=c(3, 6), cens="both") == 0.6
+)
+# loq_frac errors
+assert(
+  "loq_frac checks limit correctly with cens='left'",
+  tryCatch(
+    vpc:::loq_frac(x=1:10, limit=2:3, cens="left"),
+    error=function(e) e$message
+  ) == "limit must be a scalar if cens='left'"
+)
+assert(
+  "loq_frac checks limit correctly with cens='right'",
+  tryCatch(
+    vpc:::loq_frac(x=1:10, limit=2:3, cens="right"),
+    error=function(e) e$message
+  ) == "limit must be a scalar if cens='right'"
+)
+assert(
+  "loq_frac checks limit correctly with cens='both'",
+  tryCatch(
+    vpc:::loq_frac(x=1:10, limit=2, cens="both"),
+    error=function(e) e$message
+  ) == "limit must have 2 elements if cens='both'"
 )
